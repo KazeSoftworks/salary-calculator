d06b1e88b4c65baf139b2d1a6d22ee74d065f858
Spellkaze <spellkaze@live.com>
2021-09-28 20:45:43 -0500

    Added Employee Builder logic to generate employees on a separate module

--- components/employee.js
+++ components/employee.js
@@ -4,4 +4,5 @@ class Employee {
 		this.shifts = shifts;
 	}
 }
+
 module.exports = Employee;
--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const fileLoader = require('./util/fileLoader');
-const parser = require('./util/parser');
-const Shift = require('./components/shift');
 const Employee = require('./components/employee');
+const buildEmployee = require('./components/employeeBuilder');
 
 employeesRawData = fileLoader('data.txt')
 	.then((employeesData) => {
-		employeesData.forEach((employee) => {
-			employeeName = parser.getEmployeeName(employee);
-			shifts = [];
-			schedule = parser.getSchedule(employee);
-			schedule.forEach((shift) => {
-				shifts.push(
-					new Shift(
-						parser.getDay(shift),
-						parser.getStartShift(shift),
-						parser.getEndShift(shift)
-					)
-				);
-			});
-			console.log(new Employee(employeeName, shifts));
-		});
+		employees = employeesData.map(buildEmployee);
 	})
 	.catch((err) => {
 		console.error(err);
